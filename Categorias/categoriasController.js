3f69f3c2367801b4a7f7584cb38c43d2a052f5bb
davi-pou07 <davihareis@gmail.com>
2021-03-10 21:43:20 -0300

    VALID;EDIT;PESQUISA INDEX

--- Categorias/categoriasController.js
+++ Categorias/categoriasController.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const Categoria = require("../DataBases/Categoria")
+const Sequelize = require('sequelize')
 
+//Novo
 router.get("/admin/categoria/novo", (req, res) => {
     res.render("admin/categoria/novo")
 })
@@ -25,11 +27,51 @@ router.post("/categorias/salvar", (req, res) => {
 //Listar
 
 router.get("/admin/categorias",(req,res)=>{
-    Categoria.findAll({where:{status:true}}).then(categorias=>{
+    Categoria.findAll({order:[
+        ["id","asc"]
+    ]}).then(categorias=>{
         res.render('admin/categoria/index',{categorias:categorias})
     })
 })
 
+router.post("/categoria/find",(req,res)=>{
+    op = Sequelize.Op
+    busca = `%${req.body.busca}`
+    Categoria.findAll({where:{titulo:{[op.substring]:busca}}}).then(categorias =>{
+        res.send(categorias)
+    })
+})
+
+//Editar
+router.get("/admin/categoria/editar/:categoriaId",(req,res)=>{
+    var categoriaId = req.params.categoriaId
+    Categoria.findByPk(categoriaId).then(categoria =>{
+        res.render("admin/categoria/editar",{categoria:categoria})
+    })
+})
+router.post("/categoria/editar",(req,res)=>{
+    var categoriaId = req.body.categoriaId
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    var status = req.body.status
+    var destaque = req.body.destaque
+    if(categoriaId != undefined){
+        if(!isNaN(categoriaId)){
+            Categoria.update({
+                titulo:titulo,
+                descricao:descricao,
+                status:status,
+                destaque:destaque
+            },{where:{id:categoriaId}}).then(()=>{
+                res.redirect("/admin/categorias")
+            })
+        }else{
+            res.redirect("/erro")  
+        }
+    }else{
+        res.redirect("/erro")
+    }
+})
 
 //Teste
 router.get("/teste/valida",(req,res)=>{res.render("admin/categoria/teste")})
--- public/js/sub.js
+++ public/js/sub.js
@@ -1,6 +1,32 @@
 function sub(event, form) {
     event.preventDefault()
-    button = document.getElementById('buttom').setAttribute("disabled", "disabled");
-    form.submit()
-    return true 
+    titulo = document.getElementById('titulo').value
+    if (titulo == undefined || titulo == '') {
+        return false
+    } else {
+        button = document.getElementById('buttom').setAttribute("disabled", "disabled");
+        form.submit()
+        return true
+    }
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+'use strict'
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms)
+.forEach(function (form) {
+  form.addEventListener('submit', function (event) {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+  }, false)
+})
+})()
