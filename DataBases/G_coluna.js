02e6ca0b2ba8a9cb3ae7c919943639fd6c4b65ac
davi-pou07 <davihareis@gmail.com>
2021-05-12 12:35:39 -0300

    ok

--- DataBases/G_coluna.js
+++ DataBases/G_coluna.js
@@ -3,92 +3,16 @@ const connection = require('./database')
 const Grade = require('./Grade')
 
 const G_coluna = connection.define('g_colunas',{
-    coluna1:{
+    coluna:{
         type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna2:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna3:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna4:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna5:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna6:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna7:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna8:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna9:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    coluna10:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    codC1:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC2:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC3:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC4:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC5:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC6:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC7:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC8:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC9:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codC10:{
-        type:Sequelize.INTEGER,
         allowNull:true
     }
 })
 
 G_coluna.belongsTo(Grade)
 
-// G_coluna.sync({force:true}).then(()=>{
-//     console.log("Tabela G_coluna criada")        
-// })
+G_coluna.sync({force:true}).then(()=>{
+    console.log("Tabela G_coluna criada")        
+})
 
 module.exports = G_coluna
--- DataBases/G_linha.js
+++ DataBases/G_linha.js
@@ -2,93 +2,17 @@ const Sequelize = require('sequelize')
 const connection = require('./database')
 const Grade = require('./Grade')
 
-const G_linha = connection.define('g_linhas',{
-    linha1:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha2:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha3:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha4:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha5:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha6:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha7:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha8:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha9:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    linha10:{
-        type:Sequelize.STRING,
-        allowNull:true
-    },
-    codL1:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL2:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL3:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL4:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL5:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL6:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL7:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL8:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL9:{
-        type:Sequelize.INTEGER,
-        allowNull:true
-    },
-    codL10:{
-        type:Sequelize.INTEGER,
-        allowNull:true
+const G_linha = connection.define('g_linhas', {
+    linha: {
+        type: Sequelize.STRING,
+        allowNull: true
     }
 })
 
 G_linha.belongsTo(Grade)
 
-// G_linha.sync({force:true}).then(()=>{
-//     console.log("Tabela G_Linha criada")        
-// })
+G_linha.sync({force:true}).then(()=>{
+    console.log("Tabela G_Linha criada")        
+})
 
 module.exports = G_linha
