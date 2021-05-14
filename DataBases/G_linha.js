f655d2655932cbae291a1932f3f9b43ea7b780fc
davi-pou07 <davihareis@gmail.com>
2021-05-14 10:55:32 -0300

    ok

--- DataBases/G_linha.js
+++ DataBases/G_linha.js
@@ -87,8 +87,8 @@ const G_linha = connection.define('g_linhas',{
 
 G_linha.belongsTo(Grade)
 
-// G_linha.sync({force:true}).then(()=>{
-//     console.log("Tabela G_Linha criada")        
-// })
+G_linha.sync({force:true}).then(()=>{
+    console.log("Tabela G_Linha criada")        
+})
 
 module.exports = G_linha
