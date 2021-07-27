9a0aaa9ce2615e564529d3ae9591b3b964f95c3f
davi-pou07 <davihareis@gmail.com>
2021-07-27 20:09:40 -0300

    a

--- DataBases/DadosPagamentos.js
+++ DataBases/DadosPagamentos.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize')
+// const Sequelize = require('sequelize')
 const connection = require('./database')
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(/* ... */);
