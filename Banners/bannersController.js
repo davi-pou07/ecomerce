8372a2aac7a632cf0fdddfff2bbd0befca044e32
davi-pou07 <davihareis@gmail.com>
2021-08-18 13:05:03 -0300

    ok

--- Banners/bannersController.js
+++ Banners/bannersController.js
@@ -33,23 +33,43 @@ router.post("/admin/banners/salvar", (req, res) => {
 })
 
 router.get("/admin/banner/editar/:bannerId", (req, res) => {
+    var resp = req.query
     var bannerId = req.params.bannerId
-    console.log(bannerId)
     if (bannerId != undefined) {
-        console.log("1")
-        if (!isNaN(bannerId)) {
-            console.log("2")
+        Banners.findByPk(bannerId).then(banner => {
+            res.render("admin/banners/editar", { banner: banner, resp: resp })
+        })
+    } else {
+        res.redirect("admin/banners/adicionar.ejs")
+    }
+})
 
-            Banners.findByPk(bannerId).then(banner => {
-                res.render("admin/banners/editar", { banner: banner })
+router.post("/admin/banners/atualizar", async (req, res) => {
+    var { imagem, titulo, destaque, status, id } = req.body
+
+    if (titulo != undefined && destaque != undefined && status != undefined) {
+        var banner = await Banners.findByPk(id)
+        if (banner != undefined) {
+            if (imagem == '') {
+                imagem = banner.img
+            }
+            Banners.update({
+                img: imagem,
+                titulo: titulo,
+                destaque: destaque,
+                status: status
+            }, { where: { id: banner.id } }).then(() => {
+                res.redirect("/admin/banner/editar/" + id + "/?resp=1")
+            }).catch(err => {
+                res.redirect("/admin/banners")
+                console.log(err)
             })
         } else {
-
+            res.redirect("/admin/banner/editar/" + id + "/?resp=0")
         }
     } else {
-
+        res.redirect("/admin/banner/editar/" + id + "/?resp=0")
     }
-    res.render("admin/banners/adicionar.ejs", { resp: resp })
 })
 
 module.exports = router
