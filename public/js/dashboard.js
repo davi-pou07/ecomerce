dae9d03c63d352e17e81e2b24386f416b8b619c6
davi-pou07 <davihareis@gmail.com>
2021-09-06 22:10:13 -0300

    mudanças

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,7 +2,6 @@
 
 (function () {
   'use strict'
-
   feather.replace({ 'aria-hidden': 'true' })
 
   // Graphs
@@ -12,13 +11,10 @@
     type: 'line',
     data: {
       labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+        '01/09',
+        '02/09',
+        '03/09',
+        '04/09',
       ],
       datasets: [{
         data: [
@@ -28,13 +24,60 @@
           24003,
           23489,
           24092,
-          12034
+          12034,
+          34534,
+          5455,
+          676,
+          7879,
+          809,
         ],
+        label:"Autorizadas",
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
         borderWidth: 4,
         pointBackgroundColor: '#007bff'
+      },{
+        data:[
+          13978,
+          2345,
+          1483,
+          2003,
+          2389,
+          242,
+          12,
+          612,
+          812,
+          192,
+          102,
+          0
+        ],
+        label:"Pendentes",
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#ffff40', 
+        borderWidth: 4,
+        pointBackgroundColor: '#ffff40'
+      },{
+        data:[
+          13,
+          234,
+          13,
+          203,
+          289,
+          42,
+          12,
+          5151,
+          554,
+          4444,
+          0
+        ],
+        label:"Canceladas",
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#f34336', 
+        borderWidth: 4,
+        pointBackgroundColor: '#f34336'
       }]
     },
     options: {
@@ -50,4 +93,5 @@
       }
     }
   })
+
 })()
