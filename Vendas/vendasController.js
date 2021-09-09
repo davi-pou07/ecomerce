75c4ed13f918a5deb02b0dd472b8b1cfb7285ec3
davi-pou07 <davihareis@gmail.com>
2021-09-09 17:57:47 -0300

    amanahashds

--- Vendas/vendasController.js
+++ Vendas/vendasController.js
@@ -3,24 +3,37 @@ const router = express()
 const Sequelize = require('sequelize')
 const knex = require('../DataBases/dataBaseCL')
 const moment = require('moment')
+const Produto = require("../DataBases/Produto")
+const { Op } = require("sequelize");
+//-----------VENDAS EM PROCESSO ------------//
+router.get("/admin/vendas/processo", async (req, res) => {
+    var carrinhos = await knex("carrinhos").select().where({ status: true }).andWhere('quantidade', '>', 0).orderBy('createdAt', 'asc')
+    var clienteIds = []
+    var carrinhosIds = []
+    var datasCarrinho = []
+    var produtosIds = []
 
-//-----------VENDAS PENDENTES ------------//
-router.get("/admin/vendas/processo",async(req,res)=>{
-   var carrinhos = await knex("carrinhos").select().where({status:true}).orderBy('createdAt','asc')
-   var clienteIds = []
-   var carrinhosIds = []
-   var datas = []
+    carrinhos.forEach(carrinho => {
+        clienteIds.push(carrinho.clienteId)
+        carrinhosIds.push(carrinho.id)
+        datasCarrinho.push({ carrinhoId: carrinho.id, clienteId: carrinho.clienteId, data: moment(carrinho.updatedAt).format('DD/MM/YYYY') })
+    })
 
-   carrinhos.forEach(carrinho =>{
-       clienteIds.push(carrinho.clienteId)
-       carrinhosIds.push(carrinho.id)
-       datas.push({carrinhoId:carrinho.id,clienteId:carrinho.clienteId,data:moment(carrinho.updatedAt).format('DD/MM/YYYY')})
-   })
+    var clientes = await knex("clientes").select().whereIn('id', clienteIds)
+    var codItens = await knex("coditens").select().whereIn('carrinhoId', carrinhosIds)
 
-   var clientes = await knex("clientes").select().whereIn('id',clienteIds)
-   var codItens = await knex("coditens").select().whereIn('carrinhoId', carrinhosIds)
-   res.render("admin/vendas/pendentes",{carrinhos:carrinhos,clientes:clientes,codItens:codItens,datas:datas})
+    codItens.forEach(codItem => {
+        produtosIds.push(codItem.produtoId)
+    })
+
+    var produtos = await Produto.findAll({
+        where: {
+            id: { [Op.in]: produtosIds }
+        }
+    })
+
+    res.render("admin/vendas/processo", { carrinhos: carrinhos, clientes: clientes, codItens: codItens, datasCarrinho: datasCarrinho,produtos:produtos })
 })
-//-----------FIM VENDAS PENDENTES ------------//
+//-----------FIM VENDAS EM PROCESSO ------------//
 
 module.exports = router
