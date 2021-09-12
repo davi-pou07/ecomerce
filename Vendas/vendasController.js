6596ad855e611124714216a58484b9532aa66a9e
davi-pou07 <davihareis@gmail.com>
2021-09-12 16:12:24 -0300

    ll

--- Vendas/vendasController.js
+++ Vendas/vendasController.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 const knex = require('../DataBases/dataBaseCL')
 const moment = require('moment')
 const Produto = require("../DataBases/Produto")
-const { Op } = require("sequelize");
+const { Op, ConnectionTimedOutError } = require("sequelize");
 //-----------VENDAS EM PROCESSO ------------//
 router.get("/admin/vendas/processo", async (req, res) => {
     var carrinhos = await knex("carrinhos").select().where({ status: true }).andWhere('quantidade', '>', 0).orderBy('createdAt', 'asc')
@@ -17,6 +17,7 @@ router.get("/admin/vendas/processo", async (req, res) => {
         clienteIds.push(carrinho.clienteId)
         carrinhosIds.push(carrinho.id)
         datasCarrinho.push({ carrinhoId: carrinho.id, clienteId: carrinho.clienteId, data: moment(carrinho.updatedAt).format('DD/MM/YYYY') })
+        carrinho.precoTotal = carrinho.precoTotal.toLocaleString('pt-br',{style: 'currency' ,currency: 'BRL' })
     })
 
     var clientes = await knex("clientes").select().whereIn('id', clienteIds)
@@ -24,6 +25,9 @@ router.get("/admin/vendas/processo", async (req, res) => {
 
     codItens.forEach(codItem => {
         produtosIds.push(codItem.produtoId)
+        codItem.updatedAt = moment(codItem.updatedAt).format('DD/MM/YYYY')
+        codItem.precoUnit= codItem.precoUnit.toLocaleString('pt-br',{style: 'currency' ,currency: 'BRL' })
+        codItem.precoTotalItem= codItem.precoTotalItem.toLocaleString('pt-br',{style: 'currency' ,currency: 'BRL' })
     })
 
     var produtos = await Produto.findAll({
