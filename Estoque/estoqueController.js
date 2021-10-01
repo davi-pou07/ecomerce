da62abf6530ac5f851b67bfb1e3bcd0e3c127bb5
davi-pou07 <davihareis@gmail.com>
2021-10-01 18:00:00 -0300

    wdasd

--- Estoque/estoqueController.js
+++ Estoque/estoqueController.js
@@ -109,8 +109,7 @@ router.post("/estoque/acerto", (req, res) => {
 
 router.get("/admin/estoque/estgrade/:prod", (req, res) => {
     var prod = req.params.prod
-
-    Produto.findAll({where: {gradeId: { [Op.ne]: 0 }}}).then(produtos => {
+    Produto.findAll({ where: { gradeId: { [Op.ne]: 0 } } }).then(produtos => {
         if (prod != 0) {
             Produto.findByPk(prod).then(prod => {
                 if (prod.gradeId != 0) {
@@ -139,6 +138,38 @@ router.get("/admin/estoque/estgrade/:prod", (req, res) => {
             res.render("admin/estoque/estgrade", { produtos: produtos, prod: prod, grade: grade, colunas: colunas, linhas: linhas, estoques: estoques })
         }
     })
+})
+
+router.post("/estoque/editar", async (req, res) => {
+    var estoquesIds = req.body.estoquesId
+    var erro = []
+    if (estoquesIds != undefined) {
+        var estoques = await Estoque.findAll({ where: { id: { [Op.in]: estoquesIds } } })
+        estoques.forEach(async estoque => {
+
+            if (estoque.status == true) {
+                var status = false 
+            } else {
+                var status = true 
+            }
+
+            try {
+                Estoque.update({
+                    status: status
+                }, { where: { id: estoque.id } })
+            } catch (error) {
+                console.log(error)
+                erro.push(error)
+            }
+        });
+        if (erro.length > 0) {
+            res.json({ erro: erro })
+        } else {
+            res.json({ resp: "Atualizações realizada com sucesso" })
+        }
+    } else {
+        res.json({ erro: "Não foi realizado nenhuma alteração" })
+    }
 })
 
 module.exports = router
