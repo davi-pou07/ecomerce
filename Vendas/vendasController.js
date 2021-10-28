371dbb7a167566822ec4d0e59bb5c55d137ef8f1
davi-pou07 <davihareis@gmail.com>
2021-10-27 23:39:21 -0300

    statusentrega

--- Vendas/vendasController.js
+++ Vendas/vendasController.js
@@ -11,6 +11,7 @@ const DadosPagamentosPix = require('../DataBases/DadosPagamentosPix')
 const DadosPagamentosEntrega = require('../DataBases/DadosPagamentosEntrega')
 const DadosTransicoes = require('../DataBases/DadosTransicoes')
 const DadosEntregas = require('../DataBases/DadosEntregas')
+const StatusEntregas = require("../DataBases/StatusEntrega")
 const { data } = require('jquery')
 //-----------VENDAS EM PROCESSO ------------//
 router.get("/admin/vendas/processo", async (req, res) => {
@@ -54,10 +55,10 @@ router.get("/admin/vendas/transicoes", async (req, res) => {
     var carrinhos = await knex('carrinhos').select()
     var codItens = await knex("coditens").select()
 
-
     var dadosVendas = await DadosVendas.findAll()
     var datasVendas = []
     dadosVendas.forEach(dadoVenda => {
+
         var dat = dadoVenda.updatedAt
         var data = moment(dat).format('DD/MM/YYYY')
         datasVendas.push({ data: data, dadosId: dadoVenda.dadosId })
@@ -71,18 +72,13 @@ router.get("/admin/vendas/transicoes", async (req, res) => {
         var data = moment(d).format('DD/MM/YYYY')
         datasTransicoes.push({ data: data, dadosId: transicoes.dadosId })
     })
-    console.log("Algo?0")
     var dadosPagamentos = await DadosPagamentos.findAll()
-    console.log("Algo?1")
     var datasPagamento = []
     dadosPagamentos.forEach(pagamento => {
-        console.log("Algo?3")
         var d = pagamento.updatedAt
         var data = moment(d).format('DD/MM/YYYY')
         datasPagamento.push({ data: data, dadosId: pagamento.dadosId })
     })
-    console.log("Algo?2")
-
     var dadosPagamentosPix = await DadosPagamentosPix.findAll()
     var datasPagamentoPix = []
     dadosPagamentosPix.forEach(pagamento => {
@@ -100,10 +96,11 @@ router.get("/admin/vendas/transicoes", async (req, res) => {
     })
 
     var dadosEntregas = await DadosEntregas.findAll()
-    // var datasDadosEntregas = []
-    // dadosEntregas.forEach(dadosEntrega => {
-    //     dadosEntrega.dataPrevista = moment(dadosEntrega.dataPrevista).format('DD/MM/YYYY')
-    // })
+    for (var x = 0; x < dadosEntregas.length; x++) {
+        var statusEntrega = await StatusEntregas.findOne({ where: { statusId: dadosEntregas[x].status } })
+        dadosEntregas[x].status = statusEntrega.status
+        dadosEntregas[x].valor = dadosEntregas[x].valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+    }
 
     res.render("admin/vendas/transicoes", {
         clientes: clientes,
@@ -120,7 +117,6 @@ router.get("/admin/vendas/transicoes", async (req, res) => {
         datasPagamentoPix: datasPagamentoPix,
         datasPagamentoEntrega: datasPagamentoEntrega,
         dadosEntregas: dadosEntregas,
-        // datasDadosEntregas: datasDadosEntregas
     })
 })
 //-----------FIM VENDAS ------------//
@@ -158,25 +154,25 @@ router.get("/produtos/vendas/:dadosVendasId", async (req, res) => {
                 var nome = produtos.find(p => p.id == codItem.produtoId)
 
                 descontoTotal = descontoTotal + 0
-                valorTotal = valorTotal+codItem.precoTotalItem
+                valorTotal = valorTotal + codItem.precoTotalItem
                 quantidadeTotal = quantidadeTotal + codItem.quantidade
                 var desconto = 0
-                    produto.push({
-                        id: codItem.produtoId,
-                        nome: nome.nome,
-                        precoUnit: codItem.precoUnit.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
-                        quantidade: codItem.quantidade,
-                        desconto: desconto.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
-                        valotTotalItem: codItem.precoTotalItem.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
-                    })
+                produto.push({
+                    id: codItem.produtoId,
+                    nome: nome.nome,
+                    precoUnit: codItem.precoUnit.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                    quantidade: codItem.quantidade,
+                    desconto: desconto.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                    valotTotalItem: codItem.precoTotalItem.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                })
             })
             valores = {
-                descontoTotal:descontoTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
-                valorTotal:valorTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
-                quantidadeTotal:quantidadeTotal
+                descontoTotal: descontoTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                valorTotal: valorTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                quantidadeTotal: quantidadeTotal
             }
 
-            res.json({ produtos: produto, valores:valores})
+            res.json({ produtos: produto, valores: valores, dadosId: dadoVenda.dadosId })
 
         } else {
             res.json({ erro: "Venda Inexistente" })
@@ -190,8 +186,102 @@ router.get("/produtos/vendas/:dadosVendasId", async (req, res) => {
 //-----------FIM PRODUTOS VENDAS ------------//
 
 //-----------EDIÇÃO DE VENDA ------------//
-router.get("/teste",(req,res) =>{
-    res.render("admin/vendas/edicao")
+router.get("/admin/vendas/transicoes/editar/:dadosId", async (req, res) => {
+    var dadosId = req.params.dadosId
+    try {
+        var dadoVenda = await DadosVendas.findOne({ where: { dadosId: dadosId } })
+        dadoVenda.unit_price = dadoVenda.unit_price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+
+        if (dadoVenda.status == 'P') {
+            dadoVenda.status = 'Pendente'
+        } else if (dadoVenda.status == 'A') {
+            dadoVenda.status = 'Autorizado'
+        } else if (dadoVenda.status == 'R') {
+            dadoVenda.status = 'Rejeitado'
+        } else if (dadoVenda.status == 'C') {
+            dadoVenda.status = 'Cancelado'
+        }
+
+        if (dadoVenda != undefined) {
+            var produtosIds = []
+            var produto = []
+            var descontoTotal = 0
+            var valorTotal = 0
+            var quantidadeTotal = 0
+            var totaisProdutos = 0
+
+            var cliente = await knex("clientes").select("foto", "id", "nome", "email", "isWhats", "numero").where({ id: dadoVenda.clienteId })
+            var carrinho = await knex("carrinhos").select().where({ id: dadoVenda.carrinhoId })
+            var codItens = await knex("coditens").select().where({ carrinhoId: carrinho[0].id })
+
+            codItens.forEach(codItem => {
+                produtosIds.push(codItem.produtoId)
+                totaisProdutos = totaisProdutos+1
+            })
+            var produtos = await Produto.findAll({
+                where: {
+                    id: { [Op.in]: produtosIds }
+                }
+            })
+            
+            codItens.forEach(codItem => {
+                var nome = produtos.find(p => p.id == codItem.produtoId)
+                descontoTotal = descontoTotal + 0
+                valorTotal = valorTotal + codItem.precoTotalItem
+                quantidadeTotal = quantidadeTotal + codItem.quantidade
+                var desconto = 0
+                produto.push({
+                    id: codItem.produtoId,
+                    nome: nome.nome,
+                    precoUnit: codItem.precoUnit.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                    quantidade: codItem.quantidade,
+                    desconto: desconto.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                    valotTotalItem: codItem.precoTotalItem.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                })
+            })
+            valores = {
+                descontoTotal: descontoTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                valorTotal: valorTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }),
+                quantidadeTotal: quantidadeTotal,
+                totaisProdutos:totaisProdutos
+            }
+
+            var dadosEntregas = await DadosEntregas.findOne({ where: { carrinhoId: carrinho[0].id, clienteId: cliente[0].id } })
+            dadosEntregas.valor = dadosEntregas.valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+            var statusEntrega =  await StatusEntregas.findAll()
+
+            if (dadoVenda.opcaoDePagamento == 1) {
+                dadoVenda.opcaoDePagamento = 'Pix'
+                var dadosPagamentos = await DadosPagamentosPix.findOne({ where: { dadosId: dadoVenda.dadosId } })
+            } else if (dadoVenda.opcaoDePagamento == 2) {
+                dadoVenda.opcaoDePagamento = 'Mercado Pago'
+                var dadosPagamentos = await DadosPagamentos.findOne({ where: { dadosId: dadoVenda.dadosId } })
+            } else if (dadoVenda.opcaoDePagamento == 3) {
+                dadoVenda.opcaoDePagamento = 'Pagar na Entrega'
+                var dadosPagamentos = await DadosPagamentosEntrega.findOne({ where: { dadosId: dadoVenda.dadosId } })
+            }
+            var dadosTransicoes = await DadosTransicoes.findOne({ where: { dadosId: dadoVenda.dadosId } })
+
+            res.render("admin/vendas/edicao", {
+                dadoVenda: dadoVenda,
+                cliente: cliente[0],
+                carrinho: carrinho[0],
+                codItens: codItens,
+                dadosEntregas: dadosEntregas,
+                dadosPagamentos: dadosPagamentos,
+                dadosTransicoes: dadosTransicoes,
+                produto: produto,
+                valores: valores,
+                statusEntrega:statusEntrega
+            })
+        } else {
+            console.log("Venda não encontrada")
+            res.redirect("/admin/vendas/transicoes")
+        }
+    } catch (err) {
+        console.log(err)
+        res.redirect("/admin/vendas/transicoes")
+    }
 })
 //-----------FIM EDIÇÃO DE VENDA ------------//
 
